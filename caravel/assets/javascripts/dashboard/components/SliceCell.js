23eca18f6544754e3259acbe92fbfd6c02973224
兰建华 <bread@powercapital.cn>
2016-10-13 09:17:36 +0800

    新增只读explore函数, 隐藏只读dashboard工具栏

--- caravel/assets/javascripts/dashboard/components/SliceCell.js
+++ caravel/assets/javascripts/dashboard/components/SliceCell.js
@@ -14,8 +14,8 @@ function SliceCell({ expandedSlices, removeSlice, slice }) {
           <div className="col-md-12 header">
             <span>{slice.slice_name}</span>
           </div>
-          {slice.readonly != true &&
-          <div className="col-md-12 chart-controls" data-readonly = {slice.readonly} data-isreadonly = {slice.readonly == 'true'}>
+          {slice.form_data.readonly != 'true' &&
+          <div className="col-md-12 chart-controls">
             <div className="pull-right">
               <a title="Move chart" data-toggle="tooltip">
                 <i className="fa fa-arrows drag" />
