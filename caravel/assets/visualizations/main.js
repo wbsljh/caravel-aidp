9b00cb83c6a0bfe444a13798805b0340f64e1fab
兰建华 <bread@powercapital.cn>
2016-10-20 09:56:09 +0800

    add ai_markup.js

--- caravel/assets/visualizations/main.js
+++ caravel/assets/visualizations/main.js
@@ -31,7 +31,7 @@ const vizMap = {
   ec3_barline: require('./ec3_barlinepie.js'),
   ec3_map: require('./ec3_map.js'),
   ec3_pie: require('./ec3_barlinepie.js'),
-  ai_markup: require('./markup.js'),
+  ai_markup: require('./ai_markup.js'),
   ai_table: require('./ai_table.js')
 };
 export default vizMap;
