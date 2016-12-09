f6d9edaf8556bc75bcd9779703ae4a5686aad0b9
wbsljh <wbsljh@163.com>
2016-12-09 17:26:21 +0800

    modify run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-export CARAVEL_CONFIG=config.caravel_config
+export PYTHONPATH=$(cd `dirname $0`; pwd)/caravel
 uwsgi --stop /tmp/uwsgi_caravel.pid
 uwsgi -d /var/log/uwsgi.log --ini uwsgi.ini &
